fee22c80266b27ded675464beb1dfd217ad96335
Simone <55708559+FNX-hub@users.noreply.github.com>
2021-01-23 17:00:16 +0100

    #22 #21

--- src/main/java/logic/view/boundary/ManageStudentCareerProfessor.java
+++ src/main/java/logic/view/boundary/ManageStudentCareerProfessor.java
@@ -4,21 +4,21 @@ import logic.control.ManageStudentCareer;
 
 public class ManageStudentCareerProfessor {
 	
-	public void addGrade(String classCourseSubject, Integer insertedMark, String insertedDescription, String selectedStudentName, String selectedStudentSurname) {
-		//Istanzia il controller applicativo
-		ManageStudentCareer appController = new ManageStudentCareer();
-		
-		//Richiedi l'esecuzione del caso d'uso con i parametri ricevuti dall'utente
-		appController.addGrade(classCourseSubject,insertedMark,insertedDescription,selectedStudentName,selectedStudentSurname);
-		
-	}
-	
-	public void viewClassGrade(Integer classId) {
-		//Istanzia un controller applicativo
-		ManageStudentCareer appController = new ManageStudentCareer();
-		
-		//Controlla se una classe con tale identificativo è effettivamente esistente
-		//appController.checkClassId(classId);
-		
-	}
+//	public void addGrade(String classCourseSubject, Integer insertedMark, String insertedDescription, String selectedStudentName, String selectedStudentSurname) {
+//		//Istanzia il controller applicativo
+//		ManageStudentCareer appController = new ManageStudentCareer();
+//		
+//		//Richiedi l'esecuzione del caso d'uso con i parametri ricevuti dall'utente
+//		appController.addGrade(classCourseSubject,insertedMark,insertedDescription,selectedStudentName,selectedStudentSurname);
+//		
+//	}
+//	
+//	public void viewClassGrade(Integer classId) {
+//		//Istanzia un controller applicativo
+//		ManageStudentCareer appController = new ManageStudentCareer();
+//		
+//		//Controlla se una classe con tale identificativo è effettivamente esistente
+//		//appController.checkClassId(classId);
+//		
+//	}
 }
