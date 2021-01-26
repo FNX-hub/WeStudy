aacf339c08b57aaf37701ced86537c5d4544df48
Adriano <56735158+AdrianoTrani@users.noreply.github.com>
2021-01-26 21:07:20 +0100

    Update OralAssignment.java

--- src/main/java/logic/model/OralAssignment.java
+++ src/main/java/logic/model/OralAssignment.java
@@ -1,15 +1,14 @@
 package logic.model;
 
 import java.sql.Date;
-
 public class OralAssignment extends Assignment{
 
 	public OralAssignment(String type, String description, Date creation, Date deadline) {
 		super(type,description,creation,deadline);
 	}
 	
-	public OralAssignment(String type, String description) {
-		super(type, description);
+	public OralAssignment(String type, String description, Date deadline) {
+		super(type,description,deadline);
 	}
 	
 }
