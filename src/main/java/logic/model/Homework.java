31c05cab03e56224c6844e4761e5a9aff4beccb3
Adriano <56735158+AdrianoTrani@users.noreply.github.com>
2021-01-26 21:50:35 +0100

    #21

--- src/main/java/logic/model/Homework.java
+++ src/main/java/logic/model/Homework.java
@@ -3,6 +3,7 @@ package logic.model;
 public class Homework extends Assignment{
 
 	public Homework(String type, String description) {
+		//TODO eliminare
 		super(type, description);
 	}
 	
--- src/main/java/logic/model/ProgrammedTest.java
+++ src/main/java/logic/model/ProgrammedTest.java
@@ -3,6 +3,7 @@ package logic.model;
 public class ProgrammedTest extends Assignment{
 
 	public ProgrammedTest(String type, String description) {
+		//TODO eliminare 
 		super(type, description);
 	}
 	
