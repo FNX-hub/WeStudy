bbd2ccf4702088194336da93a660d2906d65e83a
Adriano <56735158+AdrianoTrani@users.noreply.github.com>
2021-01-26 21:09:02 +0100

    Update WrittenAssignment.java

--- src/main/java/logic/model/WrittenAssignment.java
+++ src/main/java/logic/model/WrittenAssignment.java
@@ -4,12 +4,12 @@ import java.sql.Date;
 
 public class WrittenAssignment extends Assignment{
 
+	public WrittenAssignment(String type, String description, Date deadline) {
+		super(type,description,deadline);
+	}
+	
 	public WrittenAssignment(String type, String description, Date creation, Date deadline) {
 		super(type,description,creation,deadline);
 	}
 	
-	public WrittenAssignment(String type, String description) {
-		super(type, description);
-	}
-
 }
