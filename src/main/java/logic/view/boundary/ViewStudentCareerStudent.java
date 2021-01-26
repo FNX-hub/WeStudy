2761ad8d7234be931161e43330d9935bd0bda4a9
Adriano <56735158+AdrianoTrani@users.noreply.github.com>
2021-01-26 21:16:59 +0100

    Update ViewStudentCareerStudent.java

--- src/main/java/logic/view/boundary/ViewStudentCareerStudent.java
+++ src/main/java/logic/view/boundary/ViewStudentCareerStudent.java
@@ -1,79 +1,17 @@
 package logic.view.boundary;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import logic.control.SimpleLogger;
 import logic.control.ViewStudentCareer;
-import logic.model.ExtendedGrade;
-import logic.model.Grade;
+import logic.model.bean.ExtendedGrade;
 import logic.model.bean.UserBean;
 
 //@author Adriano
 public class ViewStudentCareerStudent{
 
-	public List<String> viewExtendedCareer(UserBean studentBean){
-		//Risultato returnato dal Controller applicativo
-		List<ExtendedGrade> results;
-				
-		//Risultato in un formato adeguato alla view
-		List<String> convertedResults = new ArrayList<>();
-				
-		//Istanzia il controllore del caso d'uso
+	public List<ExtendedGrade> view(UserBean studentBean) {
 		ViewStudentCareer controller = new ViewStudentCareer();
-				
-		//Esegui il caso d'uso e recupera i risultati
-		results = controller.getExtendedStudentCareer(studentBean);
-		
-		try {
-			//Converti i dati ricevuti in un formalismo adeguato alla GUI
-			for(int i=0 ; i<results.size() ; i++) {
-				convertedResults.add(results.get(i).getCoursename());
-				convertedResults.add(results.get(i).getMark().toString());
-				convertedResults.add(results.get(i).getType());
-				convertedResults.add(results.get(i).getDescription());
-				convertedResults.add(results.get(i).getDatabaseDate().toString());
-			}
-		}
-		//SE la query NON ha restituito nulla
-		//	entra in logica di errore
-		catch(NullPointerException e)
-    	{
-			//Crea un'unica tupla contenente una frase esplicativa
-			SimpleLogger.info("NullPointerException caught");
-			convertedResults.add("You");
-			convertedResults.add("don't");
-			convertedResults.add("have");
-			convertedResults.add("any");
-			convertedResults.add("grade");
-    	}
-				
-		//Restituisci i risultati alla GUI
-		return convertedResults;
-	}
-	
-	public List<String> viewCareer(UserBean studentBean) {
-		//Risultato returnato dal Controller appllicativo
-		List<Grade> results;
-		
-		//Risultato in un formato adeguato alla view
-		List<String> convertedResults = new ArrayList<>();
-		
-		//Istanzia il controllore del caso d'uso
-		ViewStudentCareer controller = new ViewStudentCareer();
-		
-		//Esegui il caso d'uso e recupera i risultati
-		results = controller.getStudentCareer(studentBean);
-				
-		//Converti i dati ricevuti in un formalismo adeguato alla GUI
-		for(int i=0 ; i<results.size() ; i++) {
-			convertedResults.add(results.get(i).getMark().toString());
-			convertedResults.add(results.get(i).getType());
-			convertedResults.add(results.get(i).getDescription());
-		}
-		
-		//Restituisci i risultati alla GUI
-		return convertedResults;
+		return controller.getExtendedStudentCareer(studentBean);
 	}
 	
 }
