e125775ca35e7506863d006aad48f4f11fbbccd4
Adriano <56735158+AdrianoTrani@users.noreply.github.com>
2021-01-30 15:16:33 +0100

    #21

--- src/test/java/test/deliverable/ParentSimpleTest.java
+++ src/test/java/test/deliverable/ParentSimpleTest.java
@@ -12,12 +12,14 @@ import logic.model.bean.StudentBean;
 import logic.model.bean.UserBean;
 
 //@author Adriano
+
+//TEST
+//Dato un Parent -> Restituisci TUTTI i suoi figli Student
 public class ParentSimpleTest {
 	
-	//TEST
-	//Dato un Parent -> Restituisci TUTTI i suoi figli Student
+	//utente non esistente
 	@Test
-	public void getAllChildren(){
+	public void getAllChildren01(){
 		//Crea un bean fittizio
 		UserBean parentBean = new UserBean(1000);
 		
@@ -30,11 +32,54 @@ public class ParentSimpleTest {
 		
 		//Stampa di controllo
 		Integer actualNumOfChildren = children.size();
-		SimpleLogger.info("DEBUG: questo Parent ha N figli: " + actualNumOfChildren );
+		SimpleLogger.info("DEBUG: questo Parent ha N figli/figlie: " + actualNumOfChildren );
 		for(int i=0 ; i<actualNumOfChildren; i++) {
 			SimpleLogger.info(">>>" + i + "/" + children.get(i).getId()  + "/" + children.get(i).getName()  + "/" +  children.get(i).getSurname());
 		}
 		
+		//Controllo
+		assertEquals(expectedNumOfChildren, actualNumOfChildren);
+	}
+	
+	//utente esistente e con figli/figlie
+	@Test
+	public void getAllChildren02() {
+		//Crea un bean fittizio
+		UserBean parentBean = new UserBean(1);
+				
+		//Variabili di controllo
+		Integer expectedNumOfChildren = 2;
+				
+		//Esegui il caso d'uso
+		RecoverUserInformation controller = new RecoverUserInformation();
+		List<StudentBean> children = controller.getAllChildren(parentBean);
+
+		Integer actualNumOfChildren = children.size();
+				
+		//Controllo
+		assertEquals(expectedNumOfChildren, actualNumOfChildren);
+	}
+	
+	//utente esistente senza figli (caso limite)
+	@Test
+	public void getAllChildren03() {
+		//Crea un bean fittizio
+		UserBean parentBean = new UserBean(8);
+						
+		//Variabili di controllo
+		Integer expectedNumOfChildren = 0;
+						
+		//Esegui il caso d'uso
+		RecoverUserInformation controller = new RecoverUserInformation();
+		List<StudentBean> children = controller.getAllChildren(parentBean);
+						
+		//Stampa di controllo
+		Integer actualNumOfChildren = children.size();
+		SimpleLogger.info("trovati figli/figlie: N: " + actualNumOfChildren );
+		for(int i=0 ; i<actualNumOfChildren; i++) {
+			SimpleLogger.info(children.get(i).getName()  + "/" +  children.get(i).getSurname());
+		}
+						
 		//Controllo
 		assertEquals(expectedNumOfChildren, actualNumOfChildren);
 	}
